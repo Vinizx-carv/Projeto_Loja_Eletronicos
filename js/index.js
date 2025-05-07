a9da5b3b6535f1542873bb8e111147695012da54
Vinicius <vincarvalho2019@gmail.com>
2025-05-07 02:16:33 -0300

    adicionai carrinho e input de busca

---
+++ js/index.js
@@ -0,0 +1,24 @@
+window.addEventListener("load", function () {
+    const preloader = document.getElementById("preloader");
+    preloader.style.opacity = "0";
+    preloader.style.transition = "opacity 0.5s ease";
+
+    setTimeout(() => {
+      preloader.style.display = "none";
+    }, 500);
+  });
+
+
+ 
+
+  document.getElementById('closeModal4').addEventListener('click', function () {
+    document.getElementById('carrinho').style.display = 'none';
+});
+  
+
+
+document.getElementById('button-carrinho').addEventListener('click', function () {
+    document.getElementById('carrinho').style.display = 'flex';
+});
+  
+
